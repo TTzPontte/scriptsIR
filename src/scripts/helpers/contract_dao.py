b90199e5f1ea6a415846d5e449abc94d6b156e04
Matheus dos Santos <matheus.santos@pontte.com.br>
2024-01-31 19:56:41 -0300

    feat: ir2023

--- src/scripts/helpers/contract_dao.py
+++ src/scripts/helpers/contract_dao.py
@@ -1,6 +1,14 @@
+import json
 import logging
 from decimal import Decimal
+from pprint import PrettyPrinter
+
+import requests
+from openpyxl import Workbook
+
 from src.scripts.helpers.dao import Dao  # Import Dao from the appropriate location
+
+pp = PrettyPrinter(indent=4).pprint
 
 LOG_FILE_PATH = 'output/create_facade.log'
 logging.basicConfig(filename=LOG_FILE_PATH, level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
@@ -11,6 +19,7 @@ class ContractsDao:
         self.dao = Dao(table_name)
         self.participants_dao = Dao(table_name='tax_ir_participants')
         self.installments_dao = Dao(table_name='tax_ir_installments')
+        self.ir_list_dao = Dao(table_name='taxReturns_ir_list')
 
     def get_contract_by_id(self, contract_id):
         key = {'contractId': contract_id}
@@ -23,30 +32,75 @@ class ContractsDao:
         return total
 
     def make_contract(self, contract_id):
+
         contract = self.get_contract_by_id(contract_id)
         if contract:
             # Extract relevant contract data
-            _id = contract.get('contract_id')
             balance = Decimal(contract.get('balance', 0))
 
             # Retrieve installments and calculate total paid
             installments = self.installments_dao.get_by_secondary_index('tax_ir_contractId_gsi', str(contract_id))
-            total_paid = self.sum_installment_values(installments)
 
             # Retrieve participants
             participants = self.participants_dao.get_by_secondary_index('tax_ir_contractId_gsi', str(contract_id))
 
+            ir_list = self.ir_list_dao.get_item({'id': contract_id})
+
             new_record = {
-                'contract_id': str(_id),
+                "total": contract['total'],
+                "development": "PONTTE - HOME EQUITY",
+                "date": ir_list['data'],
+                'contractNumber': str(contract_id),
+                "baseYear": "2023",
                 "balance": balance,
-                "total_paid": total_paid,
-                "installments": installments,
-                "participants": participants,
+                "Installments": {'items': installments},
+                "Participants": {'items': participants},
             }
 
             return new_record
         else:
             return None
+
+
+class DecimalEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, Decimal):
+            return str(obj)  # Convert Decimal to string for JSON serialization
+        return super(DecimalEncoder, self).default(obj)
+
+
+def main():
+    dao = Dao('tax_ir_balance')
+    contracts = dao.get_all()
+
+    workbook = Workbook()
+    sheet = workbook.active
+    sheet.title = "Contract Data"
+    sheet["A1"] = "ContractId"
+    sheet["B1"] = "URL"
+
+    i = 2  # Start from row 2, as the first row is for headers
+    # for contract in contracts:
+    # if i <= 12:  # Adjusted to include 100 records
+    contractId = '130431'
+    print(i)
+    new_contract_record = contracts_dao.make_contract(contractId)
+    json_data = json.dumps(new_contract_record, cls=DecimalEncoder, indent=4)
+    api_url = 'https://gse2clq54c.execute-api.us-east-1.amazonaws.com/Prod/tax-returns/pdf'
+    payload = {
+        "data": {
+            "getContractInfo": json.loads(json_data)
+        }
+    }
+    headers = {'Content-Type': 'application/json'}
+    print(json.dumps(payload))
+    response = requests.post(api_url, data=json.dumps(payload), headers=headers)
+    print(response.text)
+    # Write data to Excel sheet
+    sheet[f"A{i}"] = contractId
+    sheet[f"B{i}"] = response.text
+
+    i += 1
 
 
 if __name__ == '__main__':
@@ -56,11 +110,13 @@ if __name__ == '__main__':
     contracts_dao = ContractsDao()
 
     # Example contract data
-
+    main()
     # Get a contract by ID
-    contract_id = '130458'
-    new_contract_record = contracts_dao.make_contract(contract_id)
-    if new_contract_record:
-        print(f"Created Contract: {new_contract_record}")
-    else:
-        print(f"Contract with ID {contract_id} not found.")
+    # contract_id = '130458'
+    # new_contract_record = contracts_dao.make_contract(contract_id)
+    #
+    # if new_contract_record:
+    #     json_data = json.dumps(new_contract_record, cls=DecimalEncoder, indent=4)
+    #     print(json_data)
+    # else:
+    #     print(f"Contract with ID {contract_id} not found.")
