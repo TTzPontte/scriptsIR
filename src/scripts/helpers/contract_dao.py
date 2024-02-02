d6db3d855998a543f9e2a3b0baf2658d333424da
Matheus dos Santos <matheus.santos@pontte.com.br>
2024-02-02 12:26:26 -0300

    script that runs xlsx

--- src/scripts/helpers/contract_dao.py
+++ src/scripts/helpers/contract_dao.py
@@ -15,14 +15,14 @@ logging.basicConfig(filename=LOG_FILE_PATH, level=logging.INFO, format='%(asctim
 
 
 class ContractsDao:
-    def __init__(self, table_name='tax_ir_balance'):
+    def __init__(self, table_name='taxReturns_balance'):
         self.dao = Dao(table_name)
         self.participants_dao = Dao(table_name='tax_ir_participants')
         self.installments_dao = Dao(table_name='tax_ir_installments')
         self.ir_list_dao = Dao(table_name='taxReturns_ir_list')
 
     def get_contract_by_id(self, contract_id):
-        key = {'contractId': contract_id}
+        key = {'contract_id': contract_id}
         return self.dao.get_item(key)
 
     def sum_installment_values(self, installments):
@@ -34,6 +34,8 @@ class ContractsDao:
     def make_contract(self, contract_id):
 
         contract = self.get_contract_by_id(contract_id)
+
+        print(contract)
         if contract:
             # Extract relevant contract data
             balance = Decimal(contract.get('balance', 0))
@@ -47,7 +49,7 @@ class ContractsDao:
             ir_list = self.ir_list_dao.get_item({'id': contract_id})
 
             new_record = {
-                "total": contract['total'],
+                "total": contract.get('total_paid') or contract.get('total'),
                 "development": "PONTTE - HOME EQUITY",
                 "date": ir_list['data'],
                 'contractNumber': str(contract_id),
@@ -70,37 +72,35 @@ class DecimalEncoder(json.JSONEncoder):
 
 
 def main():
-    dao = Dao('tax_ir_balance')
+    dao = Dao('taxReturns_balance')
     contracts = dao.get_all()
-
     workbook = Workbook()
     sheet = workbook.active
     sheet.title = "Contract Data"
     sheet["A1"] = "ContractId"
     sheet["B1"] = "URL"
-
-    i = 2  # Start from row 2, as the first row is for headers
-    # for contract in contracts:
-    # if i <= 12:  # Adjusted to include 100 records
-    contractId = '130431'
-    print(i)
-    new_contract_record = contracts_dao.make_contract(contractId)
-    json_data = json.dumps(new_contract_record, cls=DecimalEncoder, indent=4)
-    api_url = 'https://gse2clq54c.execute-api.us-east-1.amazonaws.com/Prod/tax-returns/pdf'
-    payload = {
-        "data": {
-            "getContractInfo": json.loads(json_data)
+    i = 2  # Start from the second row
+    for contract in contracts:
+        contractId = contract.get('contract_id')
+        print(contractId)
+        new_contract_record = contracts_dao.make_contract(contractId)
+        json_data = json.dumps(new_contract_record, cls=DecimalEncoder, indent=4)
+        api_url = 'https://gse2clq54c.execute-api.us-east-1.amazonaws.com/Prod/tax-returns/pdf'
+        payload = {
+            "data": {
+                "getContractInfo": json.loads(json_data)
+            }
         }
-    }
-    headers = {'Content-Type': 'application/json'}
-    print(json.dumps(payload))
-    response = requests.post(api_url, data=json.dumps(payload), headers=headers)
-    print(response.text)
-    # Write data to Excel sheet
-    sheet[f"A{i}"] = contractId
-    sheet[f"B{i}"] = response.text
+        print(i)
+        headers = {'Content-Type': 'application/json'}
+        response = requests.post(api_url, data=json.dumps(payload), headers=headers)
+        print(response.text)
+        # Write data to Excel sheet
+        sheet[f"A{i}"] = contractId
+        sheet[f"B{i}"] = response.text
+        i += 1  # Increment i for the next row
 
-    i += 1
+    workbook.save("contract_data.xlsx")
 
 
 if __name__ == '__main__':
